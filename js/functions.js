3819928a2cfbdfb4d2098651aff6e66c80398207
craigcwaveformlabs <craigmclarke@gmail.com>
2015-11-02 11:00:12 +0000

    Adding blog post section

--- js/functions.js
+++ js/functions.js
@@ -27,10 +27,22 @@ $(window).scroll(function() {
 
     // $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
 
-    var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll/8);
+    var opacity = (wScroll - $('.large-window').offset().top + 800) / (wScroll/5);
 
     $('.window-tint').css({'opacity': opacity});
   }
+
+
+  if (wScroll > $('.blog-posts').offset().top - $(window).height()) {
+
+    var offset = Math.min(0, wScroll - $('.blog-posts').offset().top + $(window).height() - 350);
+
+    $('.post-1').css({'transform': 'translate(' + offset + 'px,'  + Math.abs(offset * 0.2) + 'px)'});
+
+    $('.post-3').css({'transform': 'translate(' + Math.abs(offset) + 'px,'  + Math.abs(offset * 0.2) + 'px)'});
+
+  }
+
 
 
 });
